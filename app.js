5c86993a247ca36b2136ae8f3fe1af2f2a769737
rana0731 <oexeanar8a@gmail.com>
2021-05-08 13:06:12 +0800

    finance module ruu add item hiideg bolgov

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function() { // ui -> Use Interface 
 
-    // private function
+    // (private data)
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
@@ -11,15 +11,15 @@ var uiController = (function() { // ui -> Use Interface
 
     return {
 
-        // public function
+        // (public function)
         getInput: function(){
             return {
-                type: document.querySelector(DOMstrings.inputType).value,
+                type: document.querySelector(DOMstrings.inputType).value, //inc эсвэл exp ийг буцаана.
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.ipputValue).value
             };
         },
-        // public function
+        // (public function)
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -30,25 +30,25 @@ var uiController = (function() { // ui -> Use Interface
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {
 
-    // орлого объект
+    // орлого объект (private function)
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
     
-    // зарлага объект
+    // зарлага объект (private function)
     var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    // орлого зарлага өгөгдлүүдийн объект
+    // орлого зарлага өгөгдлүүдийн объект (private data)
     var data = {
 
         // Бүх орлогууд, зарлагууд хадгалагдах массив
-        allItems: {
+        items: {
             inc: [],
             exp: []
         },
@@ -58,20 +58,48 @@ var financeController = (function() {
             inc: 0,
             exp: 0
         }
-    }
+    };
+
+    // (public funtion)
+    return {
+        addItem: function(type, desc, val) {
+            var item, id;
+
+            //  [1, 2, 5, x] x нь 5+1=6 байна. Хамгийн сүүлийн id ийг олоод түүнийг нэгээр нэмэгдүүлнэ
+            if(data.items[type].length === 0) id = 1;
+            else{ 
+                id = data.items[type][DataTransfer.items[type].length-1].id + 1;
+            }
+
+            if(type === 'inc'){
+                item = new Income(id, desc, val);
+            } else {
+                item = new Expense(id, desc, val);// type === exp
+            }
+            data.items[type].push(item);
+        },
+
+        seeData: function() {
+            return data;
+        }
+    };
+
+    
+
 
 })();
-
 
 // Программын холбогч контроллер
 var appController = (function(uiController, financeController) {
 
-    // private function
+    // (private data)
     var ctrlAddItem = function() {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-        console.log(uiController.getInput());
+        var input = uiController.getInput();
         
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        financeController.addItem(input.type, input.desc, input.val);
+
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт гаргана.
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
@@ -95,7 +123,7 @@ var appController = (function(uiController, financeController) {
         });
     };
 
-    // public function
+    // (public funtion)
     return {
         init: function() {
             console.log('Application started ...');
